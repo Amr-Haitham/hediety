087f9eab7376a389764aea41aefd8da814b760bc
Amr Shams <amrofficialacc2@gmail.com>
2024-11-28 21:36:07 +0200

    added firestor collections

--- lib/core/config/firestore_collection.dart
+++ lib/core/config/firestore_collection.dart
@@ -1,3 +1,7 @@
 class FirestoreCollectionNames {
   static const String usersCollection = "users";
+  static const String eventsCollection = "events";
+  static const String giftsCollection = "gifts";
+  static const String friendsCollection = "friends";
+  static const String pledgesCollection = "pledges";
 }
